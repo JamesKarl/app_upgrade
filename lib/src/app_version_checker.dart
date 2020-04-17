19234a910ae0484ff622ca185747f90825a8cf73
JamesKarl <jameskarl2@163.com>
2020-04-17 15:53:03 +0800

    下载app相关逻辑

--- lib/src/app_version_checker.dart
+++ lib/src/app_version_checker.dart
@@ -10,4 +10,7 @@ abstract class AppVersionChecker {
 
   ///有新版本时会回调这个接口. 确认更新返回true，其它返回false
   Future<bool> onHasNewVersion(BuildContext context, AppInfo appInfo);
+
+  ///下载app的过程，通常会显示一个带进度的下载对话框，最终返回app的下载地址。
+  Future<String> onRequireDownloadApp(BuildContext context, AppInfo appInfo);
 }
--- lib/upgrade.dart
+++ lib/upgrade.dart
@@ -1,11 +1,17 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:upgrade/src/app_info.dart';
 import 'package:upgrade/src/app_version_checker.dart';
 import 'package:upgrade/src/myb_checker.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 abstract class AppUpgrade {
+  static const _installAppMethod = "installApp";
+  static const _downloadInstallAppMethod = "downloadInstallApp";
+
   static const MethodChannel _channel = const MethodChannel('upgrade');
   static bool _upgradeDialogDisplayed = false;
 
@@ -33,6 +39,9 @@ abstract class AppUpgrade {
         _upgradeDialogDisplayed = true;
         checker.onHasNewVersion(context, appInfo).then((confirmUpgrade) {
           _installingAppInBackground = confirmUpgrade == true;
+          if (confirmUpgrade == true) {
+            _installApp(context, appInfo, checker);
+          }
         }).whenComplete(() {
           _upgradeDialogDisplayed = false;
         });
@@ -46,6 +55,67 @@ abstract class AppUpgrade {
           _upgradeDialogDisplayed = false;
         });
       }
+    }
+  }
+
+  static void _installApp(
+    BuildContext context,
+    AppInfo appInfo,
+    AppVersionChecker checker,
+  ) {
+    if (Platform.isIOS) {
+      _upgradeAppByBrowser(appInfo.downloadURL);
+    } else {
+      if (appInfo.upgradeByBrowser == true) {
+        _upgradeAppByBrowser(appInfo.downloadURL);
+      } else {
+        if (appInfo.forceUpgrade) {
+          checker
+              .onRequireDownloadApp(context, appInfo)
+              .then(_installAppDirectly);
+        } else {
+          _downloadInstallApp(appInfo.downloadURL);
+        }
+      }
+    }
+  }
+
+  ///直接安装下载好的apk
+  static Future<void> _installAppDirectly(String appLocalPath) async {
+    if (appLocalPath == null) return null;
+    try {
+      return _channel.invokeMethod(_installAppMethod, {
+        "appLocalPath": appLocalPath,
+      });
+    } catch (e) {
+      debugPrint(e);
+      return null;
+    }
+  }
+
+  ///后台下载apk，下载完成后启动安装流程
+  static Future<void> _downloadInstallApp(String downloadUrl) async {
+    try {
+      return _channel.invokeMethod(_downloadInstallAppMethod, {
+        "downloadUrl": downloadUrl,
+      });
+    } catch (e) {
+      debugPrint(e);
+      return null;
+    }
+  }
+
+  static void _upgradeAppByBrowser(String downloadUrl) {
+    try {
+      _launchUrl(downloadUrl);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  static Future _launchUrl(String url) async {
+    if (url != null && await canLaunch(url)) {
+      await launch(url);
     }
   }
 }
