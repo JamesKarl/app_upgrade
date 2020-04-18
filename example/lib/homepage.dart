a2f042a3e64397c3313fb2e969f8d663d1d34cc5
JamesKarl <jameskarl2@163.com>
2020-04-18 09:30:35 +0800

    demo ui

--- example/lib/homepage.dart
+++ example/lib/homepage.dart
@@ -7,87 +7,154 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Plugin example app'),
+        title: const Text('应用升级demo'),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(width: double.infinity),
-          RaisedButton(
-            shape: StadiumBorder(),
-            color: Colors.blue,
-            child: Text(
-              "用系统浏览器强制升级",
-              style: const TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              AppUpgrade.checkUpdate(context,
-                  checker: DemoChecker(
-                    forceUpgrade: true,
-                    installByBrowser: true,
-                  ));
-            },
-          ),
-          RaisedButton(
-            shape: StadiumBorder(),
-            color: Colors.blueAccent,
-            child: Text(
-              "用系统浏览器非强制升级",
-              style: const TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              AppUpgrade.checkUpdate(context,
-                  checker: DemoChecker(
-                    forceUpgrade: false,
-                    installByBrowser: true,
-                  ));
-            },
-          ),
-          RaisedButton(
-            shape: StadiumBorder(),
-            color: Colors.purple,
-            child: Text(
-              "应用内强制升级",
-              style: const TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              AppUpgrade.checkUpdate(context,
-                  checker: DemoChecker(
-                    forceUpgrade: true,
-                    installByBrowser: false,
-                  ));
-            },
-          ),
-          RaisedButton(
-            shape: StadiumBorder(),
-            color: Colors.purpleAccent,
-            child: Text(
-              "应用内非强制升级",
-              style: const TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              AppUpgrade.checkUpdate(context,
-                  checker: DemoChecker(
-                    forceUpgrade: false,
-                    installByBrowser: false,
-                  ));
-            },
-          ),
-          RaisedButton(
-            shape: StadiumBorder(),
-            color: Colors.black87,
-            child: Text(
-              "直接安装apk",
-              style: const TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              AppUpgrade.installAppDirectly(
-                  "/data/user/0/com.app.upgrade_example/cache/com.app.upgrade_example.apk");
-            },
-          )
+          SizedBox(height: 16),
+          buildButtons(context),
         ],
       ),
+    );
+  }
+
+  Widget buildButtons(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        SizedBox(width: double.infinity),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              shape: StadiumBorder(),
+              color: Colors.blue,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.open_in_browser,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    "强制升级",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                AppUpgrade.checkUpdate(context,
+                    checker: DemoChecker(
+                      forceUpgrade: true,
+                      installByBrowser: true,
+                    ));
+              },
+            ),
+            RaisedButton(
+              shape: StadiumBorder(),
+              color: Colors.blueAccent,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.open_in_browser,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    "非强制升级",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                AppUpgrade.checkUpdate(context,
+                    checker: DemoChecker(
+                      forceUpgrade: false,
+                      installByBrowser: true,
+                    ));
+              },
+            ),
+          ],
+        ),
+        SizedBox(height: 8),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              shape: StadiumBorder(),
+              color: Colors.purple,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.apps,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    "强制升级",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                AppUpgrade.checkUpdate(context,
+                    checker: DemoChecker(
+                      forceUpgrade: true,
+                      installByBrowser: false,
+                    ));
+              },
+            ),
+            RaisedButton(
+              shape: StadiumBorder(),
+              color: Colors.purpleAccent,
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.apps,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 8.0),
+                  Text(
+                    "非强制升级",
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              onPressed: () {
+                AppUpgrade.checkUpdate(context,
+                    checker: DemoChecker(
+                      forceUpgrade: false,
+                      installByBrowser: false,
+                    ));
+              },
+            ),
+          ],
+        ),
+        SizedBox(height: 8),
+        RaisedButton(
+          shape: StadiumBorder(),
+          color: Colors.black87,
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(
+                Icons.file_download,
+                color: Colors.white,
+              ),
+              SizedBox(width: 8.0),
+              Text(
+                "直接安装apk",
+                style: const TextStyle(color: Colors.white),
+              ),
+            ],
+          ),
+          onPressed: () {
+            AppUpgrade.installAppDirectly(
+                "/data/user/0/com.app.upgrade_example/cache/com.app.upgrade_example.apk");
+          },
+        )
+      ],
     );
   }
 }
